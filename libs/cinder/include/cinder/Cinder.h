52e16913966a86186be13398a79c2787d164027d
hperigo <hperigo@gmail.com>
2018-12-14 15:24:07 -0200

    removed boost and cinder dependencies

--- libs/cinder/include/cinder/Cinder.h
+++ libs/cinder/include/cinder/Cinder.h
@@ -1,15 +1,15 @@
 /*
  Copyright (c) 2010, The Barbarian Group
  All rights reserved.
-
+ 
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and
-	the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-	the following disclaimer in the documentation and/or other materials provided with the distribution.
-
+ 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+ the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ the following disclaimer in the documentation and/or other materials provided with the distribution.
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
@@ -18,90 +18,40 @@
  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #pragma once
 
-#include <boost/cstdint.hpp>
-#include <boost/version.hpp>
-
-#if BOOST_VERSION < 104800
-	#error "Cinder requires Boost version 1.48 or later"
+#if __clang__
+#if ! __has_include( <cstdint> )
+#error "<cstdint> is missing - Cinder requires libc++ on Mac OS X and iOS"
 #endif
+#endif
+
+#include "glm/fwd.hpp"
+
+#include <cstdint>
+
+//  CINDER_VERSION % 100 is the patch level
+//  CINDER_VERSION / 100 % 1000 is the minor version
+//  CINDER_VERSION / 100000 is the major version
+#define CINDER_VERSION        902
+#define CINDER_VERSION_STR    "0.9.2dev"
 
 namespace cinder {
-using boost::int8_t;
-using boost::uint8_t;
-using boost::int16_t;
-using boost::uint16_t;
-using boost::int32_t;
-using boost::uint32_t;
-using boost::int64_t;
-using boost::uint64_t;
+    using std::int8_t;
+    using std::uint8_t;
+    using std::int16_t;
+    using std::uint16_t;
+    using std::int32_t;
+    using std::uint32_t;
+    using std::int64_t;
+    using std::uint64_t;
+}
 
-#define CINDER_CINDER
+#include <memory>
+//#include "cinder/CinderFwd.h"
 
-#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-	#define CINDER_MSW
-#elif defined(linux) || defined(__linux) || defined(__linux__)
-	#define CINDER_LINUX
-#elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
-	#define CINDER_COCOA
-	#include "TargetConditionals.h"
-	#if TARGET_OS_IPHONE
-		#define CINDER_COCOA_TOUCH
-	#else
-		#define CINDER_MAC
-	#endif
-	// This is defined to prevent the inclusion of some unfortunate macros in <AssertMacros.h>
-	#define __ASSERTMACROS__
-#else
-	#error "cinder compile error: Unknown platform"
-#endif
-
-#define CINDER_LITTLE_ENDIAN
-
-} // namespace cinder
-
-
-#if defined( _MSC_VER ) && ( _MSC_VER >= 1600 )
-	#include <memory>
-// avoid conflict with existing openFrameworks import of shared_ptr, weak_ptr and enable_shared_from_this
-//  https://github.com/openframeworks/openFrameworks/blob/0.8.0/libs/openFrameworks/types/ofTypes.h#L12-16
-#elif defined( CINDER_COCOA ) || ( __cplusplus < 201103L )
-	#include <tr1/memory>
-	namespace std {
-		using std::tr1::shared_ptr;
-		using std::tr1::weak_ptr;		
-		using std::tr1::static_pointer_cast;
-		using std::tr1::dynamic_pointer_cast;
-		using std::tr1::const_pointer_cast;
-		using std::tr1::enable_shared_from_this;
-	}
-#else
-	#include <boost/shared_ptr.hpp>
-	#include <boost/enable_shared_from_this.hpp>
-	namespace std {
-		using boost::shared_ptr; // future-proof shared_ptr by putting it into std::
-		using boost::weak_ptr;
-		using boost::static_pointer_cast;
-		using boost::dynamic_pointer_cast;
-		using boost::const_pointer_cast;
-		using boost::enable_shared_from_this;		
-	}
-#endif
-
-#include <boost/shared_ptr.hpp> // necessary for checked_array_deleter
-using boost::checked_array_deleter;
-
-// if compiler supports r-value references, #define CINDER_RVALUE_REFERENCES
-#if defined( _MSC_VER ) && ( _MSC_VER >= 1600 )
-	#define CINDER_RVALUE_REFERENCES
-#elif defined( __clang__ )
-	#if __has_feature(cxx_rvalue_references)
-		#define CINDER_RVALUE_REFERENCES
-	#endif
-#endif
 
 // Create a namepace alias as shorthand for cinder::
 #if ! defined( CINDER_NO_NS_ALIAS )
